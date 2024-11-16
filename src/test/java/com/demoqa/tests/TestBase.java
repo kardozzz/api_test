c985602b21d612b9e6f2ecb08aea3da5941b50fe
alabzin <kard33@mail.ru>
2024-11-16 19:27:56 +0300

    fixed build.gradle

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -13,10 +13,11 @@ public class TestBase {
         RestAssured.baseURI = "https://demoqa.com";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = System.getProperty("browserSize");
-        Configuration.remote = System.getProperty("remoteURL");
-        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browser = System.getProperty("browserName");
         Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.remote = "https://" + System.getProperty("login") + "@" + System.getProperty("remote");
 
     }
     @AfterEach
